89f4b70c9d7f558d715b6adcceda518f848d5fe6
chenzhiyong <172471067@qq.com>
2022-02-09 18:12:27 +0800

    优化内存拷贝和内存赋值函数.

--- core/base/osstring.c
+++ core/base/osstring.c
@@ -8,25 +8,45 @@
 void *osMemSet(void *s, os_byte_t ch, os_size_t n)
 {
 	stringLog("%s:%s:%d\n", __FILE__, __func__, __LINE__);
-	os_size_t divisor = n / sizeof(int);
-	{
-		int *ps = (int *)s;
-		int va = 0;
-		for (os_size_t i = 0; i < sizeof(int); i++)
-		{
-			va |= (int)ch << 8 * i;
-		}
-		for (os_size_t i = 0; i < divisor; i++)
-		{
-			ps[i] = va;
-		}
-	}
-	
+	if (n < sizeof(long))
 	{
 		os_byte_t *ps = (os_byte_t *)s;
-		for (os_size_t i = divisor * sizeof(int); i < n; i++)
+		for (os_size_t i = 0; i < n; i++)
 		{
 			ps[i] = ch;
+		}
+	}
+	else
+	{
+		os_size_t align = (os_size_t)s % sizeof(long) > 0 ? sizeof(long) - (os_size_t)s % sizeof(long) : 0;
+		{
+			os_byte_t *ps = (os_byte_t *)s;
+			for (os_size_t i = 0; i < align; i++)
+			{
+				ps[i] = ch;
+			}
+		}
+
+		os_size_t divisor = (n - align) / sizeof(long);
+		{
+			long *ps = (long *)((os_byte_t *)s + align);
+			long va = 0;
+			for (os_size_t i = 0; i < sizeof(long); i++)
+			{
+				va |= (long)ch << 8 * i;
+			}
+			for (os_size_t i = 0; i < divisor; i++)
+			{
+				ps[i] = va;
+			}
+		}
+
+		{
+			os_byte_t *ps = (os_byte_t *)s;
+			for (os_size_t i = align + divisor * sizeof(long); i < n; i++)
+			{
+				ps[i] = ch;
+			}
 		}
 	}
 	return s;
@@ -35,22 +55,45 @@ void *osMemSet(void *s, os_byte_t ch, os_size_t n)
 void *osMemCpy(void *dest, const void *src, os_size_t n)
 {
 	stringLog("%s:%s:%d\n", __FILE__, __func__, __LINE__);
-	os_size_t divisor = n / sizeof(int);
+	os_size_t destAlign = (os_size_t)dest % sizeof(long) > 0 ? sizeof(long) - (os_size_t)dest % sizeof(long) : 0;
+	os_size_t srcAlign = (os_size_t)src % sizeof(long) > 0 ? sizeof(long) - (os_size_t)src % sizeof(long) : 0;
+	if (n < sizeof(long) || destAlign != srcAlign)
 	{
-		int *pdest = (int *)dest;
-		int *psrc = (int *)src;
-		for (os_size_t i = 0; i < divisor; i++)
+		os_byte_t *pDest = (os_byte_t *)dest;
+		os_byte_t *pSrc = (os_byte_t *)src;
+		for (os_size_t i = 0; i < n; i++)
 		{
-			pdest[i] = psrc[i];
+			pDest[i] = pSrc[i];
 		}
 	}
-
+	else
 	{
-		os_byte_t *pdest = (os_byte_t *)dest;
-		os_byte_t *psrc = (os_byte_t *)src;
-		for (os_size_t i = divisor * sizeof(int); i < n; i++)
 		{
-			pdest[i] = psrc[i];
+			os_byte_t *pDest = (os_byte_t *)dest;
+			os_byte_t *pSrc = (os_byte_t *)src;
+			for (os_size_t i = 0; i < destAlign; i++)
+			{
+				pDest[i] = pSrc[i];
+			}
+		}
+
+		os_size_t divisor = (n - destAlign) / sizeof(long);
+		{
+			long *pDest = (long *)((os_byte_t *)dest + destAlign);
+			long *pSrc = (long *)((os_byte_t *)src + destAlign);
+			for (os_size_t i = 0; i < divisor; i++)
+			{
+				pDest[i] = pSrc[i];
+			}
+		}
+
+		{
+			os_byte_t *pDest = (os_byte_t *)dest;
+			os_byte_t *pSrc = (os_byte_t *)src;
+			for (os_size_t i = destAlign + divisor * sizeof(long); i < n; i++)
+			{
+				pDest[i] = pSrc[i];
+			}
 		}
 	}
 	return dest;
